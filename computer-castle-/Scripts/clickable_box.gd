04e700dfbf9680271fa5588d55688dd9a2450eff
CalebJoines <calebjoines@gmail.com>
2026-03-09 12:27:05 -0400

    Added Resource Screen

--- computer-castle-/Scripts/clickable_box.gd
+++ computer-castle-/Scripts/clickable_box.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
 @export var scene_path: String
+@export var bank: String
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
 		if event.pressed:
 			print("Box was clicked. Switching scenes...")
+			QuestionManager.set_bank(bank)
 			get_tree().change_scene_to_file(scene_path)
