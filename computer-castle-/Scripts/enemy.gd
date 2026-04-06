d7d03f2b790d94a9e1c043d4fcbb31bb7ef1d77c
Wyatt Anderson <Wyattdanderson1@gmail.com>
2026-04-06 10:55:45 -0400

    added death sound to enemies

--- computer-castle-/Scripts/enemy.gd
+++ computer-castle-/Scripts/enemy.gd
@@ -4,7 +4,7 @@ class_name Enemy
 @export var max_hp: int = 10
 @export var move_speed: float = 50
 @export var damage_to_base: int = 1
-
+@export var diesound = AudioStream
 var path_follow
 var hp: int
 
@@ -16,6 +16,7 @@ func _ready():
 	add_to_group("enemies")
 	path_follow = get_parent()
 	hp = max_hp
+
 	
 func _process(delta: float) -> void:
 	path_follow.progress += move_speed * delta
@@ -31,6 +32,13 @@ func take_damage(amount: int):
 		die()
 
 func die():
+	var player = AudioStreamPlayer.new()
+	player.stream = diesound
+	get_tree().current_scene.add_child(player)
+	player.play()
+	await player.finished
+	player.queue_free()
+	
 	if path_follow:
 		path_follow.queue_free()
 	else:
--- computer-castle-/Scripts/healthbar.gd
+++ computer-castle-/Scripts/healthbar.gd
@@ -3,6 +3,9 @@ extends Node2D
 @onready var healthbar=  %AnimatedSprite2D
 var displayed_frame: float = 0
 var tween: Tween
+var sounds = {
+	"damage": preload("res://Audio/Sound_effects/Map_SFX/Damage_Warning.mp3")
+}
 
 func _ready() -> void:
 	ResourceManager.reset_health()
@@ -10,7 +13,15 @@ func _ready() -> void:
 	on_health_changed(ResourceManager.health)
 	await get_tree().create_timer(5.0).timeout
 
+func playsound(name):
+	var player = AudioStreamPlayer.new()
+	player.stream = sounds[name]
+	add_child(player)
+	player.play()
+	player.finished.connect(player.queue_free)
+
 func on_health_changed(new_health: int):
+	playsound("damage")
 	var max_frames = healthbar.sprite_frames.get_frame_count("Damage") - 1
 	var percent = float(new_health) / ResourceManager.max_health
 	var target_frame = percent * max_frames
