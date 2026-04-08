f5df805564908caa048b7204c2962778fc981149
Wyatt Anderson <Wyattdanderson1@gmail.com>
2026-04-08 11:55:49 -0400

    fixed menu bug in RP

--- computer-castle-/Scripts/spentchecker.gd
+++ computer-castle-/Scripts/spentchecker.gd
@@ -1,9 +1,11 @@
 extends Node2D
 @onready var button =%SubmitButton
+@onready var boxes =%ClickableBoxes
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if ResourceManager.get_energy() == 0:
 		button.visible=true
+		boxes.visible=false
 	else:
 		button.visible=false
