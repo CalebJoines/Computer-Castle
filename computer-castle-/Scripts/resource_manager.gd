3f444c7224edf7df7b2beb217c8b8433da52569d
Wyatt Anderson <Wyattdanderson1@gmail.com>
2026-04-30 11:09:15 -0400

    less questions

--- computer-castle-/Scripts/resource_manager.gd
+++ computer-castle-/Scripts/resource_manager.gd
@@ -2,11 +2,11 @@ extends Node
 @onready var scenepath= "res://Scenes/loss_screen.tscn"
 
 # RESOURCE VALUES
-var volts : int = 90
-var data : int = 90
-var circuits : int = 90
-var bandwidth : int = 90
-var ai_cores : int = 90
+var volts : int = 0
+var data : int = 0
+var circuits : int = 00
+var bandwidth : int = 0
+var ai_cores : int = 0
 var energy : int = 5
 var health: int = 23:
 	set(value):
@@ -132,7 +132,7 @@ func reset_resources():
 	bandwidth=0
 	data=0
 	health=max_health
-	energy = ((LevelManager.get_current_level_index())*5)+10
+	energy = ((LevelManager.get_current_level_index())*5)+5
 	
 #healthbar stuff
 func take_damage(damage:int):
